9c73c28e5ab144f7ee1c654191b79271756af84c
emraan <emraanali11@gmail.com>
2014-11-23 17:22:56 +0530

    Added comments for first functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the inverse of Matrix
+## We are creating 2 functions as defined below to calculate the inverse of Matrix 
+## using caching to reduce the computation.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object, which will return a list
+## It will set, get values of vector and also set,get value of mean.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+    invx <- NULL
+    set <- function(y) {
+      x <<- y
+      invx <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  }
 
 
 ## Write a short comment describing this function
